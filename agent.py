1d308df5b415c6d541c83459f8936b1fb335daae
Ren1990 <kongrenhwai@hotmail.com>
2024-07-28 15:45:11 +0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -98,7 +98,7 @@ with body:
     with col1:
         st.subheader("About Myself...")
         st.write("[Linkedin](https://www.linkedin.com/in/renhwai-kong/)")
-        st.write("[Github](https://github.com/Ren1990/house_price_reg_model)")
+        st.write("[Github](https://github.com/Ren1990?tab=repositories)")
         st.write("[Tableau](https://public.tableau.com/app/profile/kyloren.kong/viz/Demo_2024InvestmentPortfolio/DBPortfolio)")
         st.image("assets/image1.jpg", width=360)
         st.write('Hi! This is me, Ren Hwai, chilling in Iceland. Happy family trip during my career break! After working in top US semicond company for 8 years as Senior Technology Development Process Engineer & Smart Manufacturing Analyst (Eng. IV), I take a long break to sharpen my Python skill in data science & analysis, and study for CFA (Chartered Finance Analyst) to look for new industry exposure and work opportunity.')
