27c59937d52a7660594a88be0782cb28049086e0
Ren1990 <kongrenhwai@hotmail.com>
2024-07-28 15:50:54 +0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -97,14 +97,13 @@ with body:
 
     with col1:
         st.subheader("About Myself...")
-        st.write("My [Linkedin](https://www.linkedin.com/in/renhwai-kong/) Profile")
-        st.write("Visit my [Github](https://github.com/Ren1990?tab=repositories) projects")
-        st.write("Take a look on [Tableau](https://public.tableau.com/app/profile/kyloren.kong/viz/Demo_2024InvestmentPortfolio/DBPortfolio) viz")
         st.image("assets/image1.jpg", width=360)
         st.write('Hi! This is me, Ren Hwai, chilling in Iceland. Happy family trip during my career break!')
+        st.write("My [Linkedin](https://www.linkedin.com/in/renhwai-kong/) Profile")
+        st.write("Visit my [Github](https://github.com/Ren1990?tab=repositories) projects")
+        st.write("Take a look on [Tableau](https://public.tableau.com/app/profile/kyloren.kong/viz/Demo_2024InvestmentPortfolio/DBPortfolio) viz")        
         st.write('After working in top US semicond company for 8 years as Sr. Business Analyst and Process Development Engineer, I took a long break to sharpen my Python skill in data science & analysis, and study for CFA (Chartered Finance Analyst) to look for new industry exposure and work opportunity.')
                  
-        
     with col3:
         st.subheader("Job Description")
         job_summary=''
