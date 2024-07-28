8914e095756428ab3304964cddf1ec8f484b03ab
Ren1990 <kongrenhwai@hotmail.com>
2024-07-28 17:35:03 +0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -120,8 +120,9 @@ with body:
         st.write(job_summary)
 
 if "messages" not in st.session_state:
-    st.session_state.messages = []
     st.session_state.table=update_knowledge()
+    st.session_state.messages = []
+    
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
