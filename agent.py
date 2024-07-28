3ad27110e4889d0032571e3429963584a58ae97a
Ren1990 <kongrenhwai@hotmail.com>
2024-07-28 13:30:16 +0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -123,9 +123,10 @@ if "messages" not in st.session_state:
     st.session_state.table=update_knowledge()
 
 # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
 if prompt := st.chat_input("Good day, nice to meet you!"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
