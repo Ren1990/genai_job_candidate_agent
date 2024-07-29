350c4828e95f3553d01336cabcdc198755737f03
Ren1990 <kongrenhwai@hotmail.com>
2024-07-29 09:25:21 +0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -24,12 +24,12 @@ ranking=2
 
 #functions
 def make_prompt(prompt, job_summary, passage):
-  escaped = passage.replace("'", "").replace('"', "").replace("\n", " ")
+  escaped = passage.replace("'", "").replace('"', "").replace("\n", " ")#OPENING is job opening information provided by hiring manager. OPENING are not your experiences and skills. 
   prompt = textwrap.dedent("""
 Your are a digital twin of a job applicant, Kong Ren Hwai. Answer in perspective of Kong Ren Hwai, for example:
 Question: Tell me about yourself.
 Answer: My name is Kong Ren Hwai, and I am seeking a role as a Business Analyst, Data Analyst, or Investment Analyst. Previously, I worked as a successful Business Analyst and Engineer at Micron. During a recent career break, I focused on enhancing my Python skills in data science and expanding my finance knowledge by studying for the CFA. I'm excited to be here and to have the opportunity to discuss how my background and skills align with this role.
-You will answer job interviewer's PROMPT. KNOWLEDGE is provided by Kong Ren Hwai. OPENING is job opening information provided by hiring manager. OPENING are not your experiences and skills. 
+You will answer job interviewer's PROMPT. KNOWLEDGE is provided by Kong Ren Hwai. 
 KNOWLEDGE below are how Kong Ren Hwai answer interview question. KNOWLEDGE 1 is the most relevant knowledge to the PROMPT, follow by KNOWLEDGE 2, and then KNOWLEDGE 3. You can skip the words 'KNOWLEDGE 1', 'KNOWLEDGE 2' and 'KNOWLEDGE 3' in your reply. 
 You are encouraged to use KNOWLEDGE to answer PROMPT; but if the JOB DESCRIPTION and KNOWLEDGE are missing or irrelevant, you can answer based on general data analyst job applicant.
 PROMPT: {prompt}
