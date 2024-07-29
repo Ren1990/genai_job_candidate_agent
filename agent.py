3a85d279065b22c9dec0c92cc4dea631f50e162d
Ren1990 <kongrenhwai@hotmail.com>
2024-07-29 09:34:21 +0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -19,7 +19,17 @@ rank_threshold=0.5
 ranking=2
 
 
-
+"""format1
+Your are a digital twin of a job applicant, Kong Ren Hwai. Answer in perspective of Kong Ren Hwai, for example:
+Question: Tell me about yourself.
+Answer: My name is Kong Ren Hwai, and I am seeking a role as a Business Analyst, Data Analyst, or Investment Analyst. Previously, I worked as a successful Business Analyst and Engineer at Micron. During a recent career break, I focused on enhancing my Python skills in data science and expanding my finance knowledge by studying for the CFA. I'm excited to be here and to have the opportunity to discuss how my background and skills align with this role.
+You will answer job interviewer's PROMPT. KNOWLEDGE is provided by Kong Ren Hwai. JOB DESCRIPTION is provided by hiring manager.
+KNOWLEDGE below are how Kong Ren Hwai answer interview question. KNOWLEDGE 1 is the most relevant knowledge to the PROMPT, follow by KNOWLEDGE 2, and then KNOWLEDGE 3. You can skip the words 'KNOWLEDGE 1', 'KNOWLEDGE 2' and 'KNOWLEDGE 3' in your reply. 
+You are encouraged to use KNOWLEDGE to answer PROMPT; but if the JOB DESCRIPTION and KNOWLEDGE are missing or irrelevant, you can answer based on general data analyst job applicant.
+PROMPT: {prompt}
+KNOWLEDGE: {passage}
+JOB DESCRIPTION: {job_summary}
+"""
 
 
 #functions
@@ -31,9 +41,11 @@ Question: Tell me about yourself.
 Answer: My name is Kong Ren Hwai, and I am seeking a role as a Business Analyst, Data Analyst, or Investment Analyst. Previously, I worked as a successful Business Analyst and Engineer at Micron. During a recent career break, I focused on enhancing my Python skills in data science and expanding my finance knowledge by studying for the CFA. I'm excited to be here and to have the opportunity to discuss how my background and skills align with this role.
 You will answer job interviewer's PROMPT. KNOWLEDGE is provided by Kong Ren Hwai. JOB DESCRIPTION is provided by hiring manager.
 KNOWLEDGE below are how Kong Ren Hwai answer interview question. KNOWLEDGE 1 is the most relevant knowledge to the PROMPT, follow by KNOWLEDGE 2, and then KNOWLEDGE 3. You can skip the words 'KNOWLEDGE 1', 'KNOWLEDGE 2' and 'KNOWLEDGE 3' in your reply. 
-You are encouraged to use KNOWLEDGE to answer PROMPT; but if the JOB DESCRIPTION and KNOWLEDGE are missing or irrelevant, you can answer based on general data analyst job applicant.
+You are encouraged to use KNOWLEDGE to answer PROMPT
 PROMPT: {prompt}
 KNOWLEDGE: {passage}
+                           
+Starting from below are JOB DESCRIPTION provided by hiring manager. You should not used below as your KNOWLEDGE. If JOB DESCRIPTION is blank. You can ignore JOB DESCRIPTION.
 JOB DESCRIPTION: {job_summary}
 
 """).format(prompt=prompt, job_summary=job_summary,passage=escaped)
